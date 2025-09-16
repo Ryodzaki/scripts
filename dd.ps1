3a0f398b92ab15c58f31d86df7bb76750c0ff800
Glam <brillianteledemone@gmail.com>
2025-09-16 18:52:29 +0300

    a new dd script
    HELLO

---
+++ dd.ps1
@@ -0,0 +1,23 @@
+Invoke-WebRequest -Uri "https://github.com/ZaikoARG/xxstrings/releases/download/1.0.0/xxstrings64.exe" -OutFile "xxstrings64.exe"
+
+
+$puk = (Get-Process -Name javaw).Id
+
+
+foreach($smallpuk in $puk)
+{
+    Write-Host "Analyzing PID: $smallpuk"
+
+    $output = (.\xxstrings64.exe -p $smallpuk | Out-String) -split "`n"
+        
+    foreach($out in $output) 
+    {
+        if ($out.Contains("OgUwQPNl"))
+        {
+            Write-Host "Glam" -ForegroundColor Pink
+        }
+    }
+}
+
+
+Remove-Item -Path "xxstrings64.exe" -Force
